11039a6d1f18025015f124c92650f0f64f63db61
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-08-03 23:07:43 +0530

    test1

--- app/src/main/java/com/example/myapplication/MHotelAddSummary.java
+++ app/src/main/java/com/example/myapplication/MHotelAddSummary.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MHotelAddSummary extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MHotelAddSummary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mhotel_add_summary);
+    }
+
+    public void sumback(View view){
+        Intent direct = new Intent(this,MAddHotel.class);
+        startActivity(direct);
+    }
+
+    public void sumsubmit(View view){
+        Intent direct = new Intent(this,MHome.class);
+        startActivity(direct);
     }
 }
