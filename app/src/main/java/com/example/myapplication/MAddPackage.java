92df3236b63f4f3649b345a6bad90b19ac774089
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-08-03 22:37:42 +0530

    test1

--- app/src/main/java/com/example/myapplication/MAddPackage.java
+++ app/src/main/java/com/example/myapplication/MAddPackage.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MAddPackage extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MAddPackage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madd_package);
+    }
+
+    public void morePackages(View view){
+        Intent direct = new Intent(this,MAddPackage.class);
+        startActivity(direct);
+    }
+
+    public void back(View view){
+        Intent direct = new Intent(this,MAddHotel.class);
+        startActivity(direct);
     }
 }
