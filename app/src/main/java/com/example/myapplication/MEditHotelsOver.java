585edbb1a8f3654eb02ee8507473e914ac6f84e5
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-08-04 23:42:40 +0530

    test1

--- app/src/main/java/com/example/myapplication/MEditHotelsOver.java
+++ app/src/main/java/com/example/myapplication/MEditHotelsOver.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MEditHotelsOver extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MEditHotelsOver extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medit_hotels_over);
+    }
+    public void toHomeM(View view){
+        Intent redirect = new Intent(this,MHome.class);
+        startActivity(redirect);
     }
 }
