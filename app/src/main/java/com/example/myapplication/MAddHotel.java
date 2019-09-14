9bfd9043c96077432808c11f7ec3a85b4bcd408c
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-09-14 23:28:22 +0530

    test1

--- app/src/main/java/com/example/myapplication/MAddHotel.java
+++ app/src/main/java/com/example/myapplication/MAddHotel.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MAddHotel extends AppCompatActivity {
 
@@ -32,6 +37,34 @@ public class MAddHotel extends AppCompatActivity {
         h1 = new Hotel();
 
         dbref = FirebaseDatabase.getInstance().getReference().child("Hotel");
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()) {
+                    Hid = (dataSnapshot.getChildrenCount());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        addHotel.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int tempGrade=Integer.parseInt(Hgrade.getText().toString().trim());
+
+                h1.setHname(Hname.getText().toString().trim());
+                h1.setHadress(Haddress.getText().toString().trim());
+                h1.setHdistrict(Hdistrict.getText().toString().trim());
+                h1.setHgrade(tempGrade);
+
+                dbref.child(String.valueOf(Hid+1)).setValue(h1);
+                Toast.makeText(MAddHotel.this,"Hotel Added Successfully!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
     public void toAddPackage(View view){
         Intent direct = new Intent(this,MAddPackage.class);
